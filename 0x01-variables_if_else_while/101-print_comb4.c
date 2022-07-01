b8b7a471bbee5aeaed44f242e064a4e8ee3de35d
Neba27 <bnebiat@gmail.com>
2022-07-01 13:21:05 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,91 +1,41 @@
 #include <stdio.h>
-
-
-
+#include <stdlib.h>
 /**
- *
- *  * main - print triple combos
- *
- *   *
- *
- *    * Description: print triple digit combos
- *
- *     *
- *
- *      * Return: Always 0 (Success)
- *
+ * main - main block
+ * Return: 0
  **/
-
-
-
 int main(void)
-
 {
+	int c;
+	int d;
+	int e = 0;
 
-		int i, j, k;
+	while (e < 10)
+	{
+		d = 0;
+		while (d < 10)
+		{
+			c = 0;
+			while (c < 10)
+			{
+				if (c != d && d != e && e < d && d < c)
+				{
+					putchar('0' + e);
+					putchar('0' + d);
+					putchar('0' + c);
+					if (c + d + e != 9 + 8 + 7)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				c++;
+			}
+			d++;
+		}
+		e++;
+	}
+	putchar('\n');
+	return (0);
 
-
-
-			i = 48;
-
-				j = 48;
-
-					k = 48;
-
-
-
-						while (i < 58)
-
-								{
-
-											j = i + 1;
-
-													while (j < 58)
-
-																{
-
-																				k = j + 1;
-
-																								while (k < 58)
-
-																													{
-
-																																			putchar(i);
-
-																																								putchar(j);
-
-																																													putchar(k);
-
-																																																		if (i < 55 || j < 56 || k < 57)
-
-																																																								{
-
-																																																															putchar(44);
-
-																																																																					putchar(32);
-
-																																																																										}
-
-																																																							k++;
-
-																																																											}
-
-
-
-																											j++;
-
-																													}
-
-
-
-															i++;
-
-																}
-
-
-
-							putchar(10);
-
-
-
-								return (0);
+}
