8ed121b0e5f1ec0f4581d0cfdebd4e0e03f27845
Neba27 <bnebiat@gmail.com>
2022-07-05 11:06:08 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -7,14 +7,19 @@
  */
 
 int _islower(int c)
+
 {
-	if(c >= 'a' && c <= 'z')
-    {
-        return (1);
-    }
-	else
-	{
-	return (0);
-	}
+if(c >= 'a' && c <= 'z')
+
+{
+return (1);
+}
+
+else
+
+{
+return (0);
+}
+
 }
 
