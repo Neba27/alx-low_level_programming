8c1a6c188601eb08bcdf1f96292c821e1ef3b429
Neba27 <bnebiat@gmail.com>
2022-07-05 11:51:38 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,10 +2,10 @@
 
 /**
  * print_sign - check the code.
- * @n - value to be checked
+ * @n: - value to be checked
  * return: 1 if +ve value
  * return: 0 if zero value
- * Return: -1 if -ve value
+ * Return: "-1" if -ve value
  */
 int print_sign(int n)
 {
@@ -14,12 +14,12 @@ if (n > 0)
 _putchar ('+');
 return (1);
 }
-else if ((n = 0))
+else if (n == 0)
 {
 _putchar ('0');
 return (0);
 }
-else 
+else
 _putchar ('-');
 return (-1);
 }
